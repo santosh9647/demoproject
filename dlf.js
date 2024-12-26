17f9aaced12ec66643ce1acf8761d88cdc694a4a
yogeshb002 <yogesh.bhola22@gmail.com>
2024-12-26 13:52:50 +0530

    dlf file code modification

---
+++ dlf.js
@@ -0,0 +1,121 @@
+import React from 'react'
+import "../styles/globals.css";
+
+const dlf = () => {
+    return (
+        <div>
+            <div className='px-4 py-2 w-full flex justify-between h[104px] bg-black text-white text-center items-center lg:text-[20px]'>
+                <div className=' flex h-[89px] items-center'>
+                    <img src='images\DLF\dlf_logo.png' className='w-[50px] h-[34px] s'></img>
+                </div>
+                <div className='bg-slate-500 flex  px-2 py-1 rounded-xl items-center'>
+                    <img src='images\Phone.png' className='w-40px] h-[40px]'></img>
+                    <p className=''>+91-9821964871</p>
+                </div>
+            </div>
+            <div>
+                <img src='images\DLF\dlf-privana-west.jpg.png' className='w-full h-[211px]'></img>
+            </div>
+            <div className='items-center text-center p-4'>
+                <button className='bg-black text-white rounded-lg p-2'>ENQUIRE NOW</button>
+            </div>
+            <div className='text-[10px] text-center'>
+                <p>From sprawling havens to soaring icons, each DLF community whispers a unique story.</p>
+                <p>We design with you in mind, using a customer-centric approach to craft </p>
+                <p>residences that reflect your aspirations. Step into a world where individuality thrives within </p>
+                <p>a vibrant tapestry of shared spaces.</p>
+            </div>
+            <div className='font-bold p-4 text-[20px] text-center'>
+                <p>PROJECTS IN GURGAON</p>
+            </div>
+            <div className='w-full overflow-x-scroll whitespace-nowrap flex gap-4 p-4'>
+                <div className="bg-[url('/images/DLF/Link.png')] bg-cover bg-center h-64 shrink-0 w-full">
+                    <div className='text-[12px]'>
+                        <p className='text-golden'>₹6.50 Cr Onwards</p>
+                        <p className='text-white font-bold'>DLF PRIVANA WEST</p>
+                        <p className='text-white'>Sector 76 Gurugram</p>
+                    </div>
+                </div>
+                <div className="bg-[url('/images/DLF/Link-1.png')] bg-cover bg-center h-64 w-full shrink-0">
+                    <div className='text-[12px]'>
+                        <p className='text-golden'>₹6 Cr Onwards</p>
+                        <p className='text-white font-bold'>DLF PRIVANA SOUTH</p>
+                        <p className='text-white'>Sector 77 Gurugram</p>
+                    </div>
+                </div>
+                <div className="bg-[url('/images/DLF/Link-2.png')] bg-cover bg-center h-64 w-full shrink-0">
+                    <div className='text-[12px]'>
+                        <p className='text-golden'>₹3.50 Cr Onwards</p>
+                        <p className='text-white font-bold'>DLF IMPERIAL RESIDENCES</p>
+                        <p className='text-white'>Sector 76 Gurugramm</p>
+                    </div>
+                </div>
+            </div>
+            <div className='font-bold p-4 text-[20px] text-center'>
+                <p>PROJECTS IN NEW GURGAON</p>
+            </div>
+            <div className='w-full overflow-x-scroll whitespace-nowrap flex gap-4 p-4'>
+                <div className="bg-[url('/images/DLF/Link-3.png')] bg-cover bg-center h-64 shrink-0 w-full">
+                    <div className='text-[12px]'>
+                        <p className='text-golden'>₹2.75 - 3.90 Cr</p>
+                        <p className='text-white font-bold'>DLF ULTIMA</p>
+                        <p className='text-white'>Sector 81, New Gurugram</p>
+                    </div>
+                </div>
+                <div className="bg-[url('/images/DLF/Link-4.png')] bg-cover bg-center h-64 shrink-0 w-full">
+                    <div className='text-[12px]'>
+                        <p className='text-golden'>₹2.25 - 3.40 Cr</p>
+                        <p className='text-white font-bold'>DLF PRIMUS</p>
+                        <p className='text-white'>Sector 82A, New Gurugram</p>
+                    </div>
+                </div>
+                <div className="bg-[url('/images/DLF/Link-5.png')] bg-cover bg-center h-64 shrink-0 w-full">
+                    <div className='text-[12px]'>
+                        <p className='text-golden'>₹1.50 - 2.50 Cr</p>
+                        <p className='text-white font-bold'>DLF REGAL GARDENS</p>
+                        <p className='text-white'>Sector 90, New Gurugram</p>
+                    </div>
+                </div>
+            </div>
+            <div className='font-bold p-4 text-[20px] text-center '>
+                <p>PROJECTS IN NEW GURGAON</p>
+            </div>
+            <div className='w-full overflow-x-scroll whitespace-nowrap flex gap-4 p-4'>
+                <div className="bg-[url('/images/DLF/Link-6.png')] bg-cover bg-center h-64 shrink-0 w-full">
+                    <div className='text-[12px]'>
+                        <p className='text-golden'>DLF BELAIRE</p>
+                        <p className='text-white'>Sector 54, Phase 5 Gurugram</p>
+                    </div>
+                </div>
+                <div className="bg-[url('/images/DLF/Link-7.png')] bg-cover bg-center h-64 shrink-0 w-full">
+                    <div className='text-[12px]'>
+                        <p className='text-golden'>DLF CREST</p>
+                        <p className='text-white'>Sector 54, Phase 5 Gurugram</p>
+                    </div>
+                </div>
+                <div className="bg-[url('/images/DLF/Link-8.png')] bg-cover bg-center h-64 shrink-0 w-full">
+                    <div className='text-[12px]'>
+                        <p className='text-golden'>DLF PARK PLACE</p>
+                        <p className='text-white'>Sector 54, Phase 5 Gurugram</p>
+                    </div>
+                </div>
+            </div>
+            <div className='items-center text-center p-4'>
+                <button className='bg-black text-white rounded-lg p-2'>ENQUIRE NOW</button>
+            </div>
+            <div className='bg-[#F0F5FB] w-full h-[198px] text-center items-center pt-8'>
+                <p>We take pride in the communities we developed.</p>
+                <p>From planned suburbs to exceptional sky-</p>
+                <p>scrapers, DLF uses a customer-centric approach</p>
+                <p>to develop</p>
+                <p>homes of the highest standard which stand the</p>
+                <p>test of time.</p>
+            </div>
+            <div className='items-center text-center p-4'>
+            <button className='bg-slate-500 text-white rounded-lg p-2'>Call Now   |   Request a Call Back</button> 
+            </div>
+        </div>
+    )
+}
+
+export default dlf
